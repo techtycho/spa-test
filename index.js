913342fca7df7540b3c81ba0347c8321494ca2ae
techtycho <omarahmedabdelhakim@gmail.com>
2023-06-05 12:41:06 +0300

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+function route(event) {
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+}
+
+const routes = {
+  404: "/templates/404.html",
+  "/": "/templates/home.html",
+  "/about": "/templates/about.html",
+  "/lorem": "/templates/lorem.html",
+};
+
+function handleLocation() {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  console.log(route);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", route, true);
+  xhr.onload = function () {
+    document.querySelector("#main-page").innerHTML = this.responseText;
+  };
+
+  xhr.send();
+}
+
+document.querySelectorAll("a").forEach((elem) => {
+  elem.addEventListener("click", route);
+});
+
+window.onpopstate = handleLocation;
+handleLocation();
